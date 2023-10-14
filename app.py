f4905ab3043d8909c5352fdc85e58bdd7dab8f9f
Nathan <nathan.laoue@gmail.com>
2023-10-14 16:08:41 +0200

    correct indentation

--- app.py
+++ app.py
@@ -22,18 +22,18 @@ if __name__ == "__main__":
         with st.chat_message("user"):
             st.markdown(prompt)
 
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        # for response in openai.ChatCompletion.create(
-        #     model=st.session_state["openai_model"],
-        #     messages=[
-        #         {"role": m["role"], "content": m["content"]}
-        #         for m in st.session_state.messages
-        #     ],
-        #     stream=True,
-        # ):
-        #     full_response += response.choices[0].delta.get("content", "")
-        #     message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = "...```print('hello world !')```"
+            # for response in openai.ChatCompletion.create(
+            #     model=st.session_state["openai_model"],
+            #     messages=[
+            #         {"role": m["role"], "content": m["content"]}
+            #         for m in st.session_state.messages
+            #     ],
+            #     stream=True,
+            # ):
+            #     full_response += response.choices[0].delta.get("content", "")
+            #     message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
