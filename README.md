84ada4f6dd6f966697c556e1f14213e17ca138d1
Nathan <nathan.laoue@gmail.com>
2023-10-14 16:04:32 +0200

    init

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Chat-UI
 
-A web app built with streamlit to get a chat ui.
+A web app built with streamlit to get a chat ui. The app is designed to run with vllm so the openai secret key and base api have to be changed according to vllm docs.
+
+Run the app :
+```bash
+streamlit run app.py
+```
---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import openai
+
+
+if __name__ == "__main__":
+    st.title("COPAIN")
+    openai.api_key = "EMPTY"
+    openai.api_base = "http://localhost:8000/v1"
+
+    if "model" not in st.session_state:
+        st.session_state["model"] = "Mistral-7B-Instruct-v0.1"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Envoyez un message"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        # for response in openai.ChatCompletion.create(
+        #     model=st.session_state["openai_model"],
+        #     messages=[
+        #         {"role": m["role"], "content": m["content"]}
+        #         for m in st.session_state.messages
+        #     ],
+        #     stream=True,
+        # ):
+        #     full_response += response.choices[0].delta.get("content", "")
+        #     message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
